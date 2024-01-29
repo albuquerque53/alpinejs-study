b3ab929e237a2f8f7497d5f16bc9a234fe0add51
Gabriel Albuquerque <albuquerque.se@proton.me>
2024-01-29 18:20:17 -0300

    feat: basic modal

---
+++ 12-simple-modal/scripts.js
@@ -0,0 +1,35 @@
+function setup() {
+    return {
+        showModal: false,
+        title: 'Oi',
+        content: 'oasad',
+
+        handleModalEvent(e) {
+            this.title = e.detail.title;
+            this.content = e.detail.content;
+
+            this.showModal = true;
+        }
+    }
+}
+
+function formData() {
+    return {
+        title: '',
+        content: '',
+        
+        submit() {
+            modalEvent = new CustomEvent(
+                'modal',
+                {
+                    detail: {
+                        'title' : this.title,
+                        'content': this.content
+                    }
+                }
+            );
+
+            window.dispatchEvent(modalEvent);
+        }
+    }
+}
